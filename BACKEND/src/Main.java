891e52efec98dfeb9de1df9865a27ffbc4133f5c
abaasaaddams <abaasacollins@gmail.com>
2024-09-19 22:47:32 +0300

    mono dimentional and multi dimentional arrays

--- BACKEND/src/Main.java
+++ BACKEND/src/Main.java
@@ -6,10 +6,33 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        int i;
+        for (i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        int[] age = new int[]{4, 5, 6, 7};
+        System.out.println(age[3]);
+
+        for (i = 0; i < age.length; i++) {
+            System.out.print(age[i] );}
+        System.out.println();
+
+
+            for (int value : age) {
+                System.out.print(value);
+            }
+        System.out.println();
+         int [][] number =new int [][]{{23,45,56},
+            {98,37,78},
+                 {45,37,78},};
+            for (int h=0; h < 3; h++){for (int j=0; j<3; j++){
+                System.out.println(number[h][j]+"\t");
+            }
+                System.out.println();
+
+            }
+        }
     }
-}
+
